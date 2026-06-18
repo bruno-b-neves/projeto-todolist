59b0695b15224941c94ba84afe42f77f4eb362ee
BRUNO BARBOSA NEVES <brunobningles@gmail.com>
2026-06-18 17:53:06 -0300

    todoList api

---
+++ todolist/src/test/java/br/com/brunoneves/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoneves.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
